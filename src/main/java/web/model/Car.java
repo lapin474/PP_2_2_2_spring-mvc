4ebc08be543d7ab15d1d0344aaa905fb35c694a3
test java project <danilair@icloud.com>
2025-04-23 11:10:55 +0300

    added car

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,47 @@
+package web.model;
+
+
+import java.util.List;
+
+public class Car {
+    private Long id;
+    private String model;
+    private int series;
+
+    public Car() {}
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return model + " " + series;
+    }
+
+}
