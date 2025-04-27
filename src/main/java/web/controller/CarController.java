95463afcdf31a959ccb0fcdf7ec2a53c3cd0b031
test java project <danilair@icloud.com>
2025-04-27 15:59:08 +0300

    added car?

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 
 @Controller
@@ -20,8 +21,12 @@ public class CarController {
         this.carDao = carDao;
     }
     @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("cars", carDao.getCars());
+    public String index(@RequestParam(value = "count", required = false) Integer count, Model model) {
+        if (count == null || count >= 5) {
+            model.addAttribute("cars", carDao.getCars());
+        } else {
+            model.addAttribute("cars", carDao.getCars().stream().limit(count).toList());
+        }
         return "cars";
     }
     @GetMapping("/{id}")
